355b59aecba4a36758adb653aa405e089b319fb9
camkol <koliyus3@hotmail.com>
2024-08-16 13:57:38 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import React from "react";
+import { LongitudeLatitudeDisplay } from "./components/LongitudeLatitudeDisplay.js";
+import { HemisphereDisplay } from "./components/HemisphereDisplay.js";
+
+function App() {
+  return (
+    <main>
+      <h1>Geo Locator</h1>
+      <LongitudeLatitudeDisplay />
+      <HemisphereDisplay />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/LongitudeLatitudeDisplay.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useGeolocation } from "../hooks/useGeolocation.js";
+export function LongitudeLatitudeDisplay() {
+  const currentLocation = useGeolocation(); // REPLACE ME!
+
+  if (!currentLocation) {
+    return <p>{"Sorry, location is currently unavailable"}</p>;
+  }
+
+  // currentLocation should have a .latitude and .longitude property
+  const { latitude, longitude } = currentLocation;
+
+  return (
+    <section>
+      <p>
+        {latitude}° {latitude > 0 ? "N" : "S"}
+      </p>
+      <hr />
+      <p>
+        {longitude}° {longitude > 0 ? "E" : "W"}
+      </p>
+    </section>
+  );
+}
